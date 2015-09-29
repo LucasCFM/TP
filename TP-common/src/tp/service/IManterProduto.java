157ebc2dd2303ba38ead537f867ebd6a1f18dd32
Pedro Silva <pedrohenrique9702@gmail.com>
2015-09-29 19:33:42 -0300

    Teste do common kct
    testei porra

--- TP-common/src/tp/service/IManterProduto.java
+++ TP-common/src/tp/service/IManterProduto.java
@@ -1,19 +1,15 @@
+package tp.service;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tp.proxy;
-
-import tp.controller.alterarFornecedor;
 
 /**
  *
  * @author pernambucanas
  */
-public class TpStub  {
-    
-    private String serverAddress;
-    private int serverPort;
+public interface IManterProduto {
     
 }
